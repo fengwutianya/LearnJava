d008dd2d4e1cfb7556398a9face6df03343980cc
fengwutianya <949979029@qq.com>
2016-08-06 22:12:27 +0800

    chat0.2 TextField TextArea WindowClosing

--- src/wechat/ChatClient.java
+++ src/wechat/ChatClient.java
@@ -2,6 +2,8 @@ package wechat;
 
 import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  * Created by xuan on 2016/8/6 0006.
@@ -20,6 +22,13 @@ public class ChatClient extends Frame{
         add(textField, BorderLayout.SOUTH);
         add(textArea, BorderLayout.NORTH);
         pack();
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                setVisible(false);
+                System.exit(0);
+            }
+        });
         setVisible(true);
     }
 }
