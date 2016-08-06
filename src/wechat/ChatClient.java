ce6e51ff7e3b1c7f8522384c60f4591bf850ee02
fengwutianya <949979029@qq.com>
2016-08-06 22:01:53 +0800

    chat0.2 TextField TextArea

--- src/wechat/ChatClient.java
+++ src/wechat/ChatClient.java
@@ -1,11 +1,15 @@
 package wechat;
 
+import javax.swing.border.Border;
 import java.awt.*;
 
 /**
  * Created by xuan on 2016/8/6 0006.
  */
 public class ChatClient extends Frame{
+    TextField textField = new TextField();
+    TextArea textArea = new TextArea();
+
     public static void main(String[] args) {
         new ChatClient().launchFrame();
     }
@@ -13,6 +17,8 @@ public class ChatClient extends Frame{
     public void launchFrame() {
         setLocation(800, 600);
         setSize(600, 600);
+        add(textField, BorderLayout.SOUTH);
+        add(textArea, BorderLayout.NORTH);
         setVisible(true);
     }
 }
