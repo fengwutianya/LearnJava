08608aea7ac049976838810b81493552209caef1
fengwutianya <949979029@qq.com>
2016-09-19 12:25:19 +0800

    线程共享同一变量 非原子性操作带来的危险

--- src/concurrency/INT.java
+++ src/concurrency/INT.java
@@ -3,7 +3,7 @@ package concurrency;
 /**
  * Created by xuan on 2016/9/19 0019.
  */
-public abstract class intGenerator {
+public abstract class INT {
     private volatile boolean cancled = false;
 
     public void cancle() {cancled = true;}
