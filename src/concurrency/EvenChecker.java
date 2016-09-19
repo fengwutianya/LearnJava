17319686b9300992baab4ec5b11e3306202329af
fengwutianya <949979029@qq.com>
2016-09-19 12:14:00 +0800

    线程共享同一变量 非原子性操作带来的危险

---
+++ src/concurrency/EvenChecker.java
@@ -0,0 +1,41 @@
+package concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by xuan on 2016/9/19 0019.
+ */
+public class EvenChecker implements Runnable {
+    private IntGenerator generator;
+    private final int id;
+
+    public EvenChecker(IntGenerator g, int ident) {
+        generator = g;
+        id = ident;
+    }
+
+    @Override
+    public void run() {
+        while (!generator.isCancled()) {
+            int val = generator.next();
+            if (val % 2 != 0) {
+                System.out.println(val + " is not even");
+                generator.cancle();
+            }
+        }
+    }
+
+    public static void test(IntGenerator gp, int count) {
+        ExecutorService exec =
+                Executors.newCachedThreadPool();
+        for (int i = 0; i < count; i++) {
+            exec.execute(new EvenChecker(gp, i));
+        }
+        exec.shutdown();
+    }
+
+    public static void test(IntGenerator gp) {
+        test(gp, 10);
+    }
+}
