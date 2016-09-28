538d8d0b44fbe2a819c976f44505041cd3220f8f
fengwutianya <949979029@qq.com>
2016-09-28 21:56:08 +0800

    java NIO Channel using steps

--- src/learnnio/TestChannel.java
+++ src/learnnio/TestChannel.java
@@ -1,31 +1,35 @@
 package learnnio;
 
+
 import java.io.RandomAccessFile;
+import java.nio.IntBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.ByteBuffer;
+import java.nio.Buffer;
 
 /**
  * Created by xuan on 2016/8/29.
  */
 public class TestChannel {
     public static void main(String[] args) throws Exception{
-        RandomAccessFile aFile = new RandomAccessFile("D://usedByJava.txt", "rw");
+        RandomAccessFile aFile =
+                new RandomAccessFile("usedByJava.txt", "rw");
         FileChannel inChannel = aFile.getChannel();
 
-        ByteBuffer buf = ByteBuffer.allocate(48);       //分配
-
-        int bytesRead = inChannel.read(buf);            //读
-        while (bytesRead != -1) {
+        ByteBuffer buf = ByteBuffer.allocate(4);
+        int bytesRead = -1;
+        while ((bytesRead = inChannel.read(buf)) != -1) {
             System.out.println("Read " + bytesRead);
-            buf.flip();                                 //翻转
+            buf.flip();
 
             while (buf.hasRemaining()) {
-                System.out.print((char)buf.get());
+                System.out.print((char) buf.get());
             }
+            System.out.println();
 
-            buf.clear();                                //清理
-            bytesRead = inChannel.read(buf);
+            buf.clear();
         }
+
         aFile.close();
     }
 }
