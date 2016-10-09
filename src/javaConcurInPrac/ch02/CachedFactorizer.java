d2d77767cd17bb39d10861a9ca2bb3ca52eed441
fengwutianya <949979029@qq.com>
2016-10-09 15:19:22 +0800

    init algorithms sort

---
+++ src/javaConcurInPrac/ch02/CachedFactorizer.java
@@ -0,0 +1,45 @@
+package javaConcurInPrac.ch02;
+
+import net.jcip.annotations.GuardedBy;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.math.BigInteger;
+
+/**
+ * Created by xuan on 2016/10/8 0008.
+ */
+public class CachedFactorizer extends StatelessFactorizer {
+    @GuardedBy("this") private BigInteger lastNumber;
+    @GuardedBy("this") private BigInteger[] lastFactors;
+    @GuardedBy("this") private long hits;
+    @GuardedBy("this") private long cacheHits;
+
+    public synchronized long getHits() { return hits; }
+    public synchronized double getCacheHitRatio() {
+        return (double) cacheHits / (double) hits;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        BigInteger i = extractFromRequest(servletRequest);
+        BigInteger[] factors = null;
+        synchronized (this) {
+            ++hits;
+            if (i.equals(lastNumber)) {
+                ++cacheHits;
+                factors = lastFactors.clone();
+            }
+        }
+        if (factors == null) {
+            factors = factor(i);
+            synchronized (this) {
+                lastNumber = i;
+                lastFactors = factors.clone();
+            }
+        }
+        encodeIntoResponse(servletResponse, factors);
+    }
+}
