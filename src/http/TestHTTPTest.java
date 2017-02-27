656fbb619b7647f603d4add22d5024d378f90f74
fengwutianya <949979029@qq.com>
2017-02-27 10:20:56 +0800

    before

--- src/http/TestHTTPTest.java
+++ src/http/TestHTTPTest.java
@@ -1,8 +1,14 @@
+package http;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by xuan on 2016/10/23 0023.
  */
 public class TestHTTPTest {
+    @org.junit.Test
+    public void main() throws Exception {
+
+    }
 
 }
