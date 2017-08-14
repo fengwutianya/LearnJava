8db99f696523c2beacbd4eabead176bdbfc935a4
fengwutianya <949979029@qq.com>
2017-08-14 12:46:28 +0800

    offer

---
+++ src/algorithms/offer/RandomListNode.java
@@ -0,0 +1,14 @@
+package algorithms.offer;
+
+/**
+ * Created by xuan on 2017/8/14 0014.
+ */
+public class RandomListNode {
+    int label;
+    RandomListNode next = null;
+    RandomListNode random = null;
+
+    RandomListNode(int label) {
+        this.label = label;
+    }
+}
