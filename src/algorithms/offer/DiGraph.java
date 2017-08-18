d6c6c40e89dd5cb844535f9bc065fa4ef870b27f
fengwutianya <949979029@qq.com>
2017-08-18 19:52:06 +0800

    hello

---
+++ src/algorithms/offer/DiGraph.java
@@ -0,0 +1,13 @@
+package algorithms.offer;
+
+import java.util.Scanner;
+
+/**
+ * Created by xuan on 8/16/17.
+ */
+public class DiGraph {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextInt());
+    }
+}
