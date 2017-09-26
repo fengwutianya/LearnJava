eca5e03223e5c18cf41aca6bf25317d15a16e178
fengwutianya <949979029@qq.com>
2017-09-26 22:48:01 +0800

    dont know

--- src/LittleTest.java
+++ src/LittleTest.java
@@ -1,24 +1,66 @@
+import package1.Father;
 import typeinfo.toys.ToyTest;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FilterInputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 /**
  * Created by xuan on 2016/8/4 0004.
  */
 public class LittleTest extends ToyTest{
     public static void main(String[] args) {
-        try {
-            throwException();
-        } catch (Exception e) {
-//            System.out.println("inside catch");
-            throw new RuntimeException("runtime exception");
-        } finally {
-            //this is gonna work anyway
-            System.out.println("inside finally");
+//        try {
+//            throwException();
+//        } catch (Exception e) {
+////            System.out.println("inside catch");
+//            throw new RuntimeException("runtime exception");
+//        } finally {
+//            //this is gonna work anyway
+//            System.out.println("inside finally");
+//        }
+//        //not exception(RuntimeException) thrown, it's gonna work
+//        System.out.println("after catch and finally");
+//        Class cl = int[].class;
+//        cl = Double[].class;
+//        cl = double[].class;
+//        cl = ToyTest.class;
+//        B.print();
+        Method[] methods = Object.class.getDeclaredMethods();
+        for (Method method: methods) {
+            System.out.println(method.getName());
         }
-        //not exception(RuntimeException) thrown, it's gonna work
-        System.out.println("after catch and finally");
     }
 
     static void throwException() throws Exception {
         throw new Exception("Exception!");
     }
+
+    public void test() throws FileNotFoundException {
+        Field[] fields = this.getClass().getDeclaredFields();
+        System.out.println(fields);
+        Class<?>[] classes = this.getClass().getInterfaces();
+        for (Class klass : classes) {
+            System.out.println(klass.getMethods());
+        }
+        this.getClass().getClassLoader();
+        FileOutputStream fos = new FileOutputStream("hello.txt");
+//        FilterInputStream fis = new FilterInputStream();  是正常类 不过构造函数是protected
+    }
+}
+
+class A {
+    public static int a;
+    public static void print() {
+        System.out.println("A: a=" + a);
+    }
+}
+
+class B extends Father {
+    public static int a = 2;
+//    public static void print() {
+//        System.out.println("B: a=" + a);
+//    }
 }
